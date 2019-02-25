2b7ea542e9fb554e482cd16e5ae593b168154627
Michael Drennen <michaeldrennen74@gmail.com>
2019-02-25 09:00:02 -0700

    Added code to get Carbon objects for previous and next market times from the MarketHours object.

--- src/Responses/Markets/MarketHours.php
+++ src/Responses/Markets/MarketHours.php
@@ -14,14 +14,33 @@ class MarketHours {
     public $date;                   //	ISO 8601	Current date
     public $opens_at;               //	ISO 8601	Acronym for thaat market
 
-    public function __construct( array $marketHours ) {
-        $this->closes_at           = Carbon::parse( $marketHours[ 'closes_at' ] );
-        $this->extended_opens_at   = Carbon::parse( $marketHours[ 'extended_opens_at' ] );
-        $this->next_open_hours     = (string)$marketHours[ 'next_open_hours' ];
-        $this->previous_open_hours = (string)$marketHours[ 'previous_open_hours' ];
-        $this->is_open             = (bool)$marketHours[ 'is_open' ];
-        $this->extended_closes_at  = Carbon::parse( $marketHours[ 'extended_closes_at' ] );
-        $this->date                = Carbon::parse( $marketHours[ 'date' ] );
-        $this->opens_at            = Carbon::parse( $marketHours[ 'opens_at' ] );
+    public $previousOpenDate;
+    public $nextOpenDate;
+
+    public function __construct(array $marketHours) {
+        $this->closes_at           = Carbon::parse($marketHours['closes_at']);
+        $this->extended_opens_at   = Carbon::parse($marketHours['extended_opens_at']);
+        $this->next_open_hours     = (string)$marketHours['next_open_hours'];
+        $this->previous_open_hours = (string)$marketHours['previous_open_hours'];
+        $this->is_open             = (bool)$marketHours['is_open'];
+        $this->extended_closes_at  = Carbon::parse($marketHours['extended_closes_at']);
+        $this->date                = Carbon::parse($marketHours['date']);
+        $this->opens_at            = Carbon::parse($marketHours['opens_at']);
+
+        // Denormalized fields
+        $this->previousOpenDate    = $this->getDateFromLink($this->previous_open_hours);
+        $this->nextOpenDate        = $this->getDateFromLink($this->next_open_hours);
     }
+
+    /**
+     * @param string $urlToHours Ex: https://api.robinhood.com/markets/XNYS/hours/2019-02-26/
+     * @return Carbon
+     */
+    protected function getDateFromLink(string $urlToHours): Carbon {
+        $urlParts   = explode('/', $urlToHours);
+        $stringDate = end($urlParts);
+        return Carbon::parse($stringDate);
+    }
+
+
 }
